ac97040905261f3c93b1bda95c66f5a430121839
AngryNeeson-52 <smileyboy@naver.com>
2023-07-20 10:50:45 +0900

    server side

---
+++ GameServer/Player.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace GameServer
+{
+    class Player
+    {
+        public int id;
+        public string username;
+
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public int hp;
+        public int max_hp = 100;
+
+        public Player(int _id, string _username, Vector3 _spawnPosition, int _hp)
+        {
+            id = _id;
+            username = _username;
+            position = _spawnPosition;
+            rotation = Quaternion.Identity;
+            hp = _hp;
+        }
+
+        public void Update()
+        {
+            ServerSend.PlayerPosition(this);
+            ServerSend.PlayerRotation(this);
+        }
+
+        public void SetPosition(Vector3 _position)
+        {
+            position = _position;
+        }
+        public void SetRotation(Quaternion _rotation)
+        {
+            rotation = _rotation;
+        }
+        public void SetHp()
+        {
+            hp = hp - 5;
+            ServerSend.PlayerHP(this);
+        }
+        public void Respawn()
+        {
+            hp = max_hp;
+            ServerSend.PlayerRespawn(this);
+        }
+
+        public void Fire()
+        {
+            ServerSend.PlayerFire(this);
+        }
+    }
+}
---
+++ GameServer/ServerHandle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+
+namespace GameServer
+{
+    class ServerHandle
+    {
+        public static void WelcomeReceived(int _fromClient, Packet _packet)
+        {
+            int _clientIdCheck = _packet.ReadInt();
+            string _username = _packet.ReadString();
+
+            Console.WriteLine($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {_fromClient} : {_username}.");
+            if (_fromClient != _clientIdCheck)
+            {
+                Console.WriteLine($"Player \"{_username}\" (ID: {_fromClient}) has assumed the wrong client ID ({_clientIdCheck})!");
+            }
+            Server.clients[_fromClient].SendIntoGame(_username);
+        }
+
+        public static void PlayerPosition(int _fromClient, Packet _packet)
+        {
+            Vector3 _position = _packet.ReadVector3();
+
+            Server.clients[_fromClient].player.SetPosition(_position);
+        }
+        public static void PlayerRotation(int _fromClient, Packet _packet)
+        {
+            Quaternion _rotation = _packet.ReadQuaternion();
+
+            Server.clients[_fromClient].player.SetRotation(_rotation);
+        }
+        public static void PlayerHit(int _fromClient, Packet _packet)
+        {
+           int _hitid = _packet.ReadInt();
+
+            Server.clients[_hitid].player.SetHp();
+        }
+        public static void PlayerRespawn(int _fromClient, Packet _packet)
+        {
+            int _spawnid = _packet.ReadInt();
+
+            Server.clients[_spawnid].player.Respawn();
+        }
+        public static void PlayerFire(int _fromClient, Packet _packet)
+        {
+            int _clientIdCheck = _packet.ReadInt();
+
+            Server.clients[_clientIdCheck].player.Fire();
+        }
+    }
+}
